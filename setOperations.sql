a160ccfa3c9772059443d30c972a4c11bb974749
Bhaskar <bkotu6717@gmail.com>
2023-05-22 15:04:53 +0530

    Update setOperations.sql

--- setOperations.sql
+++ setOperations.sql
@@ -1,3 +1,15 @@
+drop TABLE programmers;
+CREATE TABLE programmers(id INTEGER, prof1 varchar(100), prof2 varchar(100));
+
+insert into programmers VALUES(1, 'PASCAL', 'BASIC'),
+(2, 'CLIPPER', 'COBOLT'),
+(3,'COBOLT', 'DBASE'),
+(4,'C', 'DBASE'),
+(5, 'CPP','ORACLE'),
+(6,'COBOLT', 'DBASE'),
+(7, 'PASCAL', 'CLIPPER')
+
+
 SELECT prof1 from programmers
 UNION
 SELECT prof2 from programmers
