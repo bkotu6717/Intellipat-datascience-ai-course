63e63c75b4bfcb8767061d68a5030df9f30d8378
bhaskar-rao-kotu <154314320+bhaskar-rao-kotu@users.noreply.github.com>
2024-12-07 09:47:35 +0530

    Update nth_highest_salary_mysql.sql

--- MsSqlDatascientist/nth_highest_salary_mysql.sql
+++ MsSqlDatascientist/nth_highest_salary_mysql.sql
@@ -1,3 +1,4 @@
+
 -- Find the highest salary
 -- LIMIT 0 - Will skip 0 rows and brings next one record
 SELECT DISTINCT(salary)
@@ -5,12 +6,16 @@ FROM employees
 ORDER BY salary DESC
 LIMIT 0, 1;
 
+-- LIMIT 0,1 -> Offest 0 and Limit 1
+
 -- Find the second highest salary
 -- LIMIT 1 - Will skip 1 rows and brings next one record
 SELECT DISTINCT(salary)
 FROM employees
 ORDER BY salary DESC
 LIMIT 1, 1;
+-- LIMIT 1,1 -> Offest 1 and Limit 1
+
 
 -- Find the third highest salary
 -- LIMIT 2 - Will skip 2 rows and brings next one record
@@ -18,6 +23,8 @@ SELECT DISTINCT(salary)
 FROM employees
 ORDER BY salary DESC
 LIMIT 2, 1;
+-- LIMIT 2,1 -> Offest 2 and Limit 1
+
 
 -- Find the 4th highest salary
 -- LIMIT 3- Will skip 4 rows and brings next one record
@@ -25,3 +32,5 @@ SELECT DISTINCT(salary)
 FROM employees
 ORDER BY salary DESC
 LIMIT 3, 1;
+-- LIMIT 2,1 -> Offest 3 and Limit 1
+
